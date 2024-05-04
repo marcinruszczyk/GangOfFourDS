9fc3d4f5b6da6b083c6e97086ee60960b710ef52
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-04 18:28:57 +0200

    Visitor implemented

---
+++ DesignPatterns/Visitor/Program.cs
@@ -0,0 +1,10 @@
+﻿using Visitor;
+
+var objectStructure = new ObjectStructure();
+
+objectStructure.Attach(new ConcreteElementA());
+objectStructure.Attach(new ConcreteElementB());
+
+var visitor = new ConcreteVisitor();
+
+objectStructure.Accept(visitor);
