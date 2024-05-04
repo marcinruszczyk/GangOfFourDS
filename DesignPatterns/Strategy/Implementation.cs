074a36cf05912da89990aac678b79c3415623326
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-04 10:47:41 +0200

    strategy pattern implemneted

---
+++ DesignPatterns/Strategy/Implementation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public interface IExportService
+    {
+        void Export(Order order);
+       
+    }
+
+    public class JsonExportService : IExportService
+    {
+        public void Export(Order order) 
+        {
+            Console.WriteLine($"Exporting {order.Name} to Json.");
+        }
+    }
+
+
+    public class XMLExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to XML.");
+        }
+    }
+
+
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to CSV.");
+        }
+    }
+
+
+    public class Order
+    {
+        public string Customer { get; set; }
+        public int Amount { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public IExportService? ExportService { get; set; }
+
+        public Order(string customer, int amount, string name)
+        {
+            Customer = customer;
+            Amount = amount;
+            Name = name;
+        }
+
+        public void Export()
+        {
+            ExportService?.Export(this);
+        }
+
+    }
+
+}
