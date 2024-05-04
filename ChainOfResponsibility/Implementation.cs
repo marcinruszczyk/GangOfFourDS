61c90a09114ac96118129b3c936d95796d6e965e
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-04 14:14:10 +0200

    Chain of Responsibility implemented

---
+++ ChainOfResponsibility/Implementation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    interface IHandler
+    {
+        void SetNext(IHandler handler);
+        void HandleRequest(int request);
+    }
+
+    class ConcreteHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine($"{request} handled by ConcreteHandler");
+            }
+            else if (_nextHandler != null)
+            {
+                _nextHandler.HandleRequest(request);    
+            }
+            else
+            {
+                Console.WriteLine($"{request} cannot be handled");
+            }
+        }
+
+        public void SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+        }
+    }
+
+
+
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChainOfResponsibility;
+
+var handler1 = new ConcreteHandler();
+var handler2 = new ConcreteHandler();
+var handler3 = new ConcreteHandler();
+
+
+handler1.SetNext(handler2);
+handler2.SetNext(handler3);
+
+//send request to the chain
+
+handler1.HandleRequest(5);
+handler1.HandleRequest(12);
+handler1.HandleRequest(8);
+
+
