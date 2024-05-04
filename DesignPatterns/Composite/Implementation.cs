a253290ea16b262ceb5b02b43d6b4bf9d4013df1
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-04 20:39:55 +0200

    Composite implemented

---
+++ DesignPatterns/Composite/Implementation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compositer
+{
+    interface IComponent
+    {
+        void Operation();
+    }
+
+    class Leaf : IComponent
+    {
+        private string _name;
+
+        public Leaf(string name)
+        {
+            _name = name;
+        }
+        public void Operation()
+        {
+            Console.WriteLine($"Leaf {_name} operation");
+        }
+
+    }
+
+
+    class Composite : IComponent 
+    {
+        private List<IComponent> _children = new List<IComponent>();
+
+        public void Add(IComponent component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(IComponent component)
+        {
+            _children.Remove(component);
+        }
+
+        public void Operation()
+        {
+            foreach (var child in _children)
+            {
+                child.Operation();
+            }
+        }
+    }
+
+
+}
---
+++ DesignPatterns/Composite/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Compositer;
+
+
+
+var leaf1 = new Leaf("1");
+var leaf2 = new Leaf("2");
+var leaf3 = new Leaf("3");
+
+var composite = new Composite();
+
+composite.Add(leaf1);
+composite.Add(leaf2);
+
+var composite2 = new Composite();
+
+composite2.Add(leaf3);
+composite2.Add(composite);
+
+composite2.Operation();
+
+
+
+
+
