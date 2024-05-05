48e717c92818546d8222899fd5496e7d98e5127a
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-05 17:32:03 +0200

    Flyweight implemented

---
+++ DesignPatterns/Flyweight/Implementation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    interface IShape
+    {
+        void Draw(int x , int y);
+    }
+
+    class Circle : IShape
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+            Console.WriteLine("Creating Circle with radius" + radius);
+        }
+
+        public void Draw(int x , int y) 
+        {
+            Console.WriteLine($"Creating Circle at ({x}, {y}) with radius {radius}");
+        }
+    }
+
+    class ShapeFactory
+    {
+        private Dictionary<int, IShape> shapes = new Dictionary<int, IShape>();
+
+        public IShape GetCircle(int radius)
+        {
+            if (!shapes.ContainsKey(radius))
+            {
+                shapes[radius] = new Circle(radius);
+            }
+            return shapes[radius];
+        }
+    }
+
+
+
+}
---
+++ DesignPatterns/Flyweight/Program.cs
@@ -0,0 +1,13 @@
+﻿using FlyweightPattern;
+
+ShapeFactory shapeFactory = new ShapeFactory();
+
+IShape circle1 = shapeFactory.GetCircle(5);
+circle1.Draw(1, 2);
+
+IShape circle2 = shapeFactory.GetCircle(10);
+circle2.Draw(3, 4);
+
+
+IShape circle3 = shapeFactory.GetCircle(5);
+circle3.Draw(5, 6);
