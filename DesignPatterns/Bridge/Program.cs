630365651f91736c8f9a6026ee80dc6e22c89cab
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-05 16:42:37 +0200

    Bridge implemented

---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,10 @@
+﻿using Bridge;
+
+IImplementator implementorA = new ConcreteImplementatorA();
+Abstraction abstractionA = new RefinedAbstraction(implementorA);
+abstractionA.Operation();
+
+
+IImplementator implementorB = new ConcreteImplementatorB();
+Abstraction abstractionB = new RefinedAbstraction(implementorB);
+abstractionB.Operation();
