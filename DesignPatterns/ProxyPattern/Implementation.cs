c0be5ff8f1791a9721210895e96983d2515837ec
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-05 17:43:07 +0200

    proxy implemented

---
+++ DesignPatterns/ProxyPattern/Implementation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    interface ISubject
+    {
+        void Request();
+    }
+
+    class RealSubject : ISubject
+    {
+        public void Request() 
+        {
+            Console.WriteLine("RealSubject: Handling request");
+        }
+    }
+
+    class Proxy : ISubject
+    {
+        private RealSubject realSubject;
+
+        public Proxy()
+        {
+            realSubject = new RealSubject();    
+        }
+
+        public void Request() 
+        {
+            Console.WriteLine("Proxy: handling request");
+            realSubject.Request();
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
