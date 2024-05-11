b672a574e241715824b91e76c6f6ce3c80c089c4
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-11 18:06:41 +0200

    Memento

---
+++ DesignPatterns/Memento/Orginator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Orginator
+    {
+        string state;
+
+        public string State
+        {
+            get { return state; }
+            set 
+            {
+                state = value;
+                Console.WriteLine("state =" + state);
+            }
+        }
+
+
+        public Memento CreateMemento()
+        {
+            return (new Memento(state));
+        }
+
+
+        public void SetMemento(Memento memento) {
+            Console.WriteLine("Restoring state ...");
+            State = memento.State;
+        }
+
+    }
+}
