ccc9e38c2cae61f1ab5724ab7d4fa001d1acc94d
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-11 17:23:59 +0200

    iterator pattern example

--- Singleton/obj/Debug/net8.0/Singleton.AssemblyInfo.cs
+++ Singleton/obj/Debug/net8.0/Singleton.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3802a5901f2818b40d5e6492a32597408d7fe0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219ae27961757ba473366ee839e54030c415b076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
