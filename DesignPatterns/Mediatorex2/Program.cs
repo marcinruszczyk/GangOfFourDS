64465187eb698bfadb8a66a17251343295d97f06
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-11 17:46:22 +0200

    mediator2 example

---
+++ DesignPatterns/Mediatorex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Mediatorex2;
+
+ConcreteMediator m = new ConcreteMediator();
+
+ConcreteColleague1 c1 = new ConcreteColleague1(m);
+ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+m.Colleague1 = c1;
+m.Colleague2 = c2;
+
+c1.Send("How are you?");
+c2.Send("Fine, thanks");
+
+Console.ReadKey();
