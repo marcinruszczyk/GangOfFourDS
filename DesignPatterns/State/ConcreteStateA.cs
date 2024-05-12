fffad18eef3b4c02e4cb3aa3eabf1c8d33261815
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-12 02:27:16 +0200

    state imlemented

---
+++ DesignPatterns/State/ConcreteStateA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+}
