05030e8d67db3355d3a7f8c72245f29f72e78eef
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-13 17:40:38 +0200

    template mthod example

---
+++ DesignPatterns/TemplateMethod2/AbstractClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod2
+{
+    public abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+            
+                    
+
+    }
+}
