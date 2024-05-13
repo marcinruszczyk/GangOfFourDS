02b29a594e882544a8e43b4dd454ccd7b96e750a
Marcin Ruszczyk <marcin.ruszczyk@wp.pl>
2024-05-13 18:04:53 +0200

    adapter implemented

---
+++ DesignPatterns/Adapter1/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter1
+{
+    class Client
+    {
+        public void MakeRequest(ITarget target)
+        {
+            target.Request();
+        }
+    }
+}
---
+++ DesignPatterns/Adapter1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Adapter1;
+
+Adaptee adaptee = new Adaptee();
+
+ITarget adapter = new Adapter(adaptee);
+
+Client client = new Client();
+client.MakeRequest(adapter);
